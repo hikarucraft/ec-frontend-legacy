d85fbcbc7f219654d6233a97b9d1e8cf47a857b5
hikarucraft <147622480+hikarucraft@users.noreply.github.com>
2024-11-09 12:45:51 +0900

    fix: flat config is esmodule, so renamed file extension. I have no idea if this config perform correctly

---
+++ eslint.config.mjs
@@ -0,0 +1,67 @@
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint';
+
+const compat = new FlatCompat();
+
+export default tseslint.config(
+  eslintConfigPrettier,
+  ...compat.extends('plugin:import/recommended'),
+  { ignores: ['dist', 'eslint.config.mjs'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+            'object',
+            'type',
+          ],
+          pathGroups: [
+            {
+              pattern: '{react,react-dom/**,react-router-dom}',
+              group: 'builtin',
+              position: 'before',
+            },
+            {
+              pattern: '@/components/**',
+              group: 'parent',
+              position: 'before',
+            },
+            {
+              pattern: '**/**.module.css',
+              group: 'type',
+              position: 'after',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['builtin'],
+          alphabetize: {
+            order: 'asc',
+          },
+        },
+      ],
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    },
+  },
+);
